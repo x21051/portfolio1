56327a29b625d93a483cfe587603f7271ec7dd3a
x21051 <x21051xx@aitech.ac.jp>
2022-07-25 09:18:43 +0900

    Create portfolio.js

---
+++ js/portfolio.js
@@ -0,0 +1,14 @@
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+const element = document.getElementById(".image-compare");
+  
+const viewer = new ImageCompare(element).mount();
+
+
